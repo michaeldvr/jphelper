b8d1f854ce164a6dd13779e8e643b572ac1a4b71
michaeldvr <michael.devara@gmail.com>
2018-05-23 20:34:43 +0700

    initial commit

---
+++ jphelper/tests/test_jphelper.py
@@ -0,0 +1,34 @@
+import unittest
+import jphelper.tests.assets as assets
+import jphelper
+
+
+class TestJPHelper(unittest.TestCase):
+
+    def test_is_kanji(self):
+        self.assertTrue(jphelper.is_kanji('明日'))
+        self.assertFalse(jphelper.is_kanji('あした'))
+
+    def test_is_hiragana(self):
+        self.assertTrue(jphelper.is_hiragana('こんにちは'))
+        self.assertFalse(jphelper.is_hiragana('今日は'))
+
+    def test_is_katakana(self):
+        self.assertTrue(jphelper.is_katakana('プログラミング'))
+        self.assertFalse(jphelper.is_katakana('プログラミング言語'))
+
+    def test_is_kana(self):
+        self.assertTrue(jphelper.is_kana('あおいシャツ'))
+        self.assertFalse(jphelper.is_kana('青いシャツ'))
+
+    def test_match_reading(self):
+        for row in assets.match_reading_tests:
+            self.assertEqual(jphelper.match_reading(row[0], row[1], space='#', post_space='@'), row[2])
+
+    def test_kanaize(self):
+        for row in assets.kanaize:
+            self.assertEqual(jphelper.kanaize(row[0], row[1]), row[2])
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ jphelper/tests/test_number.py
@@ -0,0 +1,38 @@
+import unittest
+import jphelper.number as number
+import jphelper.tests.assets as assets
+
+
+class TestNumber(unittest.TestCase):
+
+    def test_japanize_kana(self):
+        for row in assets.japanize_kana:
+            self.assertEqual(number.to_japanese(row[0], row[1], row[2], row[3], row[4]), row[5])
+
+    def test_japanize_kanji(self):
+        for row in assets.japanize_kanji:
+            self.assertEqual(number.to_japanese(row[0], row[1], row[2], row[3], row[4]), row[5])
+
+    def test_japanize_minus(self):
+        for row in assets.japanize_minus:
+            self.assertEqual(number.to_japanese(row[0], row[1], row[2], row[3], row[4]), row[5])
+
+    def test_japanize_decimals(self):
+        for row in assets.japanize_decimals:
+            self.assertEqual(number.to_japanese(row[0], row[1], row[2], row[3], row[4]), row[5])
+
+    def test_group_unit(self):
+        self.assertEqual(number.group_unit(123), '123')
+        self.assertEqual(number.group_unit(-5320123232.32, separator='|', decimal_separator=':'), '-53|2012|3232:32')
+
+    def test_kanji_grouping(self):
+        self.assertEqual(number.kanji_grouping(-15124), '-1万5124')
+        self.assertEqual(number.kanji_grouping(-2345678.90123, use_hiragana=True, use_minus_sign=False),
+                         'マイナス234まん5678てん90123')
+
+    def test_shorten(self):
+        self.assertEqual(number.shorten(12345678987), '123.4億')
+
+
+if __name__ == '__main__':
+    unittest.main()
